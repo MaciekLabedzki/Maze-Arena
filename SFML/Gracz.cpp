85d92598d2dbaf4c2a548f0d81cb9f8973115545
MaciekLabedzki <32839540+MaciekLabedzki@users.noreply.github.com>
2017-11-13 23:15:24 +0100

    Part 4

---
+++ SFML/Gracz.cpp
@@ -0,0 +1,126 @@
+﻿#include <iostream>
+#include "node.h"
+#include "Gracz.h"
+
+
+
+void Gracz::walk_up() {
+	if (!dead) {
+		if (source.y <= 4) {
+			source.y = Up;
+		}
+		else source.y = Up_a;
+		playerImage.move(0, -SPD);
+		ruch = true;
+	}
+}
+
+void Gracz::walk_down() {
+	if (!dead) {
+		if (source.y < 4) {
+			source.y = Down;
+		}
+		else source.y = Down_a;
+		playerImage.move(0, SPD);
+		ruch = true;
+	}
+}
+
+void Gracz::walk_left() {
+	if (!dead) {
+		if (source.y <= 4) {
+			source.y = Left;
+		}
+		else source.y = Left_a;
+		playerImage.move(-SPD, 0);
+		ruch = true;
+	}
+}
+
+void Gracz::walk_right() {
+	if (!dead) {
+		if (source.y <= 4) {
+			source.y = Right;
+		}
+		else source.y = Right_a;
+		playerImage.move(SPD, 0);
+		ruch = true;
+	}
+}
+
+void Gracz::czy_ruch() {
+	if (!dead) {
+		if (ruch) {
+			if (source.x == 2)
+				source.x = 0;
+			else source.x++;
+		}
+		playerImage.setTextureRect(sf::IntRect(source.x * 60, source.y * 60, 60, 60));
+	}
+	setG(HP, ATAK, SPD);
+}
+
+void Gracz::Update() {
+	if (!dead) {
+		box.left = playerImage.getPosition().x;
+		box.top = playerImage.getPosition().y;
+
+		hitbox.left = playerImage.getPosition().x;
+		hitbox.top = playerImage.getPosition().y;
+
+	}
+	setG(HP, ATAK, SPD);
+}
+
+Gracz* Gracz::getter() { return this; }
+
+void Gracz::hit(Mob *mob) {
+	if (hitbox.intersects(mob->box)) {
+		int kierunek = source.y - 4;
+		//std::cout << "hit";
+		mob->HP -= ATAK;
+		mob->is_dead();
+		setG(HP, ATAK, SPD);
+		//mob->hited(kierunek);
+	}
+}
+void Gracz::hit(Gracz* mob) {
+	if (hitbox.intersects(mob->box)) {
+		int kierunek = source.y - 4;
+		//std::cout << "hit";
+		mob->HP -= ATAK;
+		mob->is_dead();
+		setG(HP, ATAK, SPD);
+		//mob->hited(kierunek);
+	}
+}
+
+void Gracz::hited(Mob *mob) {
+	HP -= mob->ATAK;
+	setG(HP, ATAK, SPD);
+	//std::cout << HP;
+	is_dead();
+}
+
+bool Gracz::is_dead() { //obs³uga œmierci gracza
+	if (HP <= 0) {
+		if (!pTexture.loadFromFile("grave.png"))
+			std::cout << "nie udalo sie zaladowc textury gracza" << std::endl;
+		playerImage.setTexture(pTexture);
+		source.x = 0;
+		source.y = 0;
+		playerImage.setTextureRect(sf::IntRect(source.x * 60, source.y * 60, 60, 60));
+		setG(HP, ATAK, SPD);
+		ruch = false;
+		Update();
+		//std::cout << "dead";
+		dead = true;
+		return true;
+
+	}
+	return false;
+}
+
+std::ostream & operator<< (std::ostream &wyjscie, const Gracz &s) {
+	return wyjscie << "HP: " << s.HP << std::endl << "SILA: " << s.ATAK << std::endl << "PUNKTY: " << s.PT << std::endl;
+}
---
+++ SFML/Gracz.h
@@ -0,0 +1,80 @@
+#ifndef Gracz_h
+#define Gracz_h
+
+#include<iostream>
+#include<string>
+#include<SFML/Graphics.hpp>
+#include "node.h"
+#include "Mob.h"
+
+
+enum Direction { Down, Left, Right, Up, Down_a, Left_a, Right_a, Up_a };
+class Mob;
+
+class Gracz {
+public:
+	sf::Texture pTexture;
+	sf::Sprite playerImage;
+	sf::FloatRect hitbox;
+	int HP, ATAK, PT, a, SPD;
+	sf::Vector2i source;
+	bool ruch;
+	bool dead;
+	sf::FloatRect box;
+
+	std::string g, health;
+
+	void Update();
+
+	Gracz(std::string graczTex, int x, int y, sf::Vector2i source1) {
+		playerImage.setPosition(x, y);
+		if (!pTexture.loadFromFile(graczTex))
+			std::cout << "nie udalo sie zaladowc textury gracza" << std::endl;
+		playerImage.setTexture(pTexture);
+		source = source1; 
+			dead = false;
+		box.height = 40;
+		box.width = 40;
+		box.left = playerImage.getPosition().x + 10;
+		box.top = playerImage.getPosition().y + 10;
+
+		hitbox.height = 60;
+		hitbox.width = 60;
+		hitbox.left = playerImage.getPosition().x;
+		hitbox.top = playerImage.getPosition().y;
+
+		ruch = false;
+		HP = 3;
+		ATAK = 1;
+		PT = 0;
+		a = 0;
+		SPD = 1;
+
+		health = "* * *";
+		g = "HP: " + std::to_string(HP) + "\nATAK: " + std::to_string(ATAK) + "\nSPD: " + std::to_string(SPD) + "";
+	}
+
+	void setG(int HP, int ATAK, int SPD) {
+		health = "";
+		g = "HP: " + std::to_string(HP) + "\nATAK: " + std::to_string(ATAK) + "\nSPD: " + std::to_string(SPD) + "";
+		for (int i = 0; i < HP; i++)
+			health += "* ";
+	}
+
+	void walk_up();
+	void walk_down();
+	void walk_left();
+	void walk_right();
+	void czy_ruch();
+	void hit(Mob *mob);
+	void hited(Mob *mob);
+	bool is_dead();
+	void Gracz::hit(Gracz* mob);
+	Gracz* Gracz::getter();
+
+	friend std::ostream & operator<< (std::ostream &wyjscie, const Gracz &g);
+
+
+};
+
+#endif
